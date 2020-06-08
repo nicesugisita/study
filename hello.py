92efd4a809511795c7a05db59123b56a22c27ceb
sugisita <mutjjangi@daum.net>
2020-06-08 01:04:59 -0700

    test

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 print ("TEST")
+<<<<<<< HEAD
 print ("Test for tag annotated")
 print ("commit --amend test")
 print ("test good")
 
 
+=======
+>>>>>>> parent of a2e8499... Test for tag annotated
 
